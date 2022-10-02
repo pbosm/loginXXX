597834ca780e0516dd1bf6edd1331ac7eca0b31e
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2022-10-02 19:55:03 -0300

    bd connect

--- database/conn.php
+++ database/conn.php
@@ -1,16 +1,11 @@
 <?php
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$host    = "localhost";
+$user    = "root";
+$passw   = "";
+$bd      = "bdteste";
 
-if($conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db)){
+if($conn = mysqli_connect($host, $user, $passw, $bd)){
     // echo "Conectado!!!!";
 } else {
     echo "Não foi possivel conectar ao banco de dados!!!";
