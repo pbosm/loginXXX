8e7f7f0853cea9da133e9811fa9bfae897422367
pbosm <pablo.machado@xcapitalbank.com>
2022-10-05 17:29:39 -0300

    upgrade navbar

---
+++ header/navbar.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-BR"> 
+<head>
+<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<title> Projeto – XXX</title>
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="navigantion">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class=""><img src=""></span>
+                        <span id="title" class="title">Projeto – XXX</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="restrita.php">
+                        <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                        <span class="title">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="colaboradores.php">
+                        <span class="icon"><ion-icon name="person-add-outline"></ion-icon></span>
+                        <span class="title">Cadastro de colaboradores</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="lancapontos.php">
+                        <span class="icon"><ion-icon name="cart-outline"></span>
+                        <span class="title">Lançamento de pontos</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="relatorio.php">
+                        <span class="icon"><ion-icon name="document-text-outline"></ion-icon></span>
+                        <span class="title">Relatório</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="logout.php">
+                        <span class="icon"><ion-icon name="exit-outline"></ion-icon></span>
+                        <span class="title">Sair</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div> 
+
+    <!-- main -->
+    <div class="main">
+    <div class="topbar">
+        <div class="toggle">
+        <ion-icon name="menu-outline"></ion-icon>
+        </div>
+        <!-- search-->
+        <div class="search">
+        <label>
+            <form class="form-inline" action="../busca/buscausuario.php" method="POST">
+                <input type="text" placeholder="Pesquisar usuários" name="pesquisar">
+                <ion-icon name="search-outline"></ion-icon>           
+            </form>                                               
+        </label>
+        </div>
+        <!-- userimg-->
+        <div class="user">
+        <img src="">
+        </div>
+    </div>
--- layout/restrita.php
+++ layout/restrita.php
@@ -1,78 +1,8 @@
 <?php
 require_once "../database/conn.php";
 require_once "verifica-acesso.php";
+require_once "../header/navbar.php";
 ?>
-<!DOCTYPE html>
-<html lang="pt-BR"> 
-<head>
-<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
-<title> Projeto – XXX</title>
-<link rel="stylesheet" type="text/css" href="../css/style.css">
-</head>
-<body>
-    <div class="container">
-        <div class="navigantion">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class=""><img src=""></span>
-                        <span class="title">Projeto – XXX</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="restrita.php">
-                        <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
-                        <span class="title">Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="colaboradores.php">
-                        <span class="icon"><ion-icon name="person-add-outline"></ion-icon></span>
-                        <span class="title">Cadastro de colaboradores</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="lancapontos.php">
-                        <span class="icon"><ion-icon name="cart-outline"></span>
-                        <span class="title">Lançamento de pontos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="relatorio.php">
-                        <span class="icon"><ion-icon name="document-text-outline"></ion-icon></span>
-                        <span class="title">Relatório</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="logout.php">
-                        <span class="icon"><ion-icon name="exit-outline"></ion-icon></span>
-                        <span class="title">Sair</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div> 
-    
-             <!-- main -->
-             <div class="main">
-             <div class="topbar">
-                 <div class="toggle">
-                    <ion-icon name="menu-outline"></ion-icon>
-                 </div>
-                 <!-- search-->
-                 <div class="search">
-                    <label>
-                        <form class="form-inline" action="../busca/buscausuario.php" method="POST">
-                            <input type="text" placeholder="Pesquisar usuários" name="pesquisar">
-                            <ion-icon name="search-outline"></ion-icon>           
-                        </form>                                               
-                    </label>
-                 </div>
-                 <!-- userimg-->
-                 <div class="user">
-                    <img src="">
-                 </div>
-             </div>
 
              <!-- cards -->
              <div class="cardBox">
@@ -152,6 +82,7 @@ require_once "verifica-acesso.php";
 
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
     <script>
         //add menutoggle
@@ -162,19 +93,23 @@ require_once "verifica-acesso.php";
         toggle.onclick = function()
         {
             navigantion.classList.toggle('active');
-            main.classList.toggle('active');            
+            main.classList.toggle('active');
+            $('#title').hide();
+            if (!navigantion.classList.toggle('.main')){
+                $('#title').show();
+            }
         }
 
-        //add hovered valss in selected list item
-        let list = document.querySelectorAll('.navigantion li');
-        function activeLink()
-        {
-            list.forEach((item) =>
-            item.classList.remove('hovered'));
-            this.classList.add('hovered');
-        }
-        list.forEach((item) =>
-        item.addEventListener('mouseover',activeLink));
+        // //add hovered valss in selected list item
+        // let list = document.querySelectorAll('.navigantion li');
+        // function activeLink()
+        // {
+        //     list.forEach((item) =>
+        //     item.classList.remove('hovered'));
+        //     this.classList.add('hovered');
+        // }
+        // list.forEach((item) =>
+        // item.addEventListener('mouseover',activeLink));
     </script>
 
 </body>
